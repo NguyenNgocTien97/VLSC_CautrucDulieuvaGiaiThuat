57a1e3a1468bede44938e2c60224a542560d1b78
NguyenNgocTien97 <2500113793@vanlangsaigon.edu.vn>
2026-03-16 10:24:10 +0700

    Hoan thanh bai tap ve nha Lesson 2

---
+++ DSA_Homework_S2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static int TinhTong(int n)
+    {
+        if (n == 1) return 1;
+        return n + TinhTong(n - 1);
+    }
+
+    static long giaiThuaVongLap(int n)
+    {
+        long Ketqua = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            Ketqua *= i;
+        }
+        return Ketqua;
+    }
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Nhap n de tinh tong: ");
+        int n = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"Tong S({n}) = {TinhTong(n)}");
+        Console.WriteLine($"Giai thua cua {n} la {giaiThuaVongLap(n)}");
+    }
+}
